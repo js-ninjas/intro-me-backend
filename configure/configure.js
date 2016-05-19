4e664c4942103935db93aa6d56917efdf67471df
kiransp <kiransp82@gmail.com>
2016-05-19 16:24:57 +0530

    defined methods

---
+++ configure/configure.js
@@ -0,0 +1,3 @@
+var url = "mongodb://localhost:27017/myDb";
+
+module.exports.url = url;
--- db_components/dbConnector.js
+++ db_components/dbConnector.js
@@ -1,3 +1,42 @@
-/**
- * Created by KiranSp on 5/18/16.
- */
+var configure=require('../configure/configure')
+function dbConnector() {
+
+}
+
+dbConnector.prototype.connectToDb = function () {
+    var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+    MongoClient.connect(configure.url, function (err, db) {
+        if (!err) {
+            console.log("We are connected");
+            /*var collection = db.collection('emp');
+            collection.insert({'name': 'kiran', 'comp': 'tcs'},function(err,data){
+                console.log("insertred")
+                console.log(data)
+                collection.find({}).toArray(function(err, docs) {
+
+                    console.log("Found the following records");
+                    console.log(docs);
+                    collection.updateMany({'name':'kiran'},{$set:{'comp':'XYZ'}},function(err,data){
+                        if(err) throw err
+
+                        console.log("ROews "+data)
+                        collection.find({}).toArray(function(err,data){
+                            console.log("Found the following records");
+                            console.log(docs);
+                        })
+                        db.close();
+                    })
+
+                });
+
+            });*/
+        }
+        else {
+
+            console.log(err)
+        }
+    });
+
+};
